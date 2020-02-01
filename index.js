019e1483f3699d1a06cdb25353d69bb463d36744
Afnan Sultan <sultan.afnan@gmail.com>
2020-02-01 03:06:03 -0500

    Added login page

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require('express')
 // app is the express server
 const app = express()
 
-// responds to HTTP GET requests with specified URL path
+// responds to HTTP GET requests and sends argument of send() to the server
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(index.html)
 });
 
+// 
 app.listen(8000, () => {
     console.log('Example app listening on port 8000!')
 });
