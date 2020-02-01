0c1d11dfae6ac64d684bb11ece3ee1d97be22d22
Noe <noramgar@protonmail.com>
2020-02-01 06:35:09 -0500

    connect to db

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const express = require('express')
 // app is the express server
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+//require('dotenv').config()
 
+app.use(express.json())
 // serve public files to client
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// respond with index.html when a GET request is made to the homepage
-app.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname + '/public/index.html'));
-    res.sendFile('/home/noramgar/Documents/repos/Bug-Tracker/public/index.html')
+mongoose.connect("mongodb+srv://noramgar:mypassword@cluster0-zbnhm.gcp.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('OMGWTFLOL')
 });
+
+
 
 // starts a UNIX socketand listens for connections on the given path
 app.listen(8000, () => {
